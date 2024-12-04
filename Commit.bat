eab0f833b4d90211ec927fb700282e5171a34be1
Luc Murray <lmurray55@qub.ac.uk>
2024-12-04 23:04:31 +0000

    Commit

--- Commit.bat
+++ Commit.bat
@@ -3,7 +3,7 @@ git add .
 git commit -m "Commit"
 git remote add origin https://github.com/DistributedAssignment/PeerToPeer.git
 git branch -M main
-git pull main
+git pull origin main
 git fetch -u origin main
 git push -u origin main
 pause
