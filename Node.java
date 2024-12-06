52ae5ef86d965b934c2291df6c7c1452d515a2db
Luc Murray <lmurray55@qub.ac.uk>
2024-12-06 17:45:31 +0000

    Commit

--- Node.java
+++ Node.java
@@ -469,6 +469,7 @@ public class Node {
 				(new Messenger(message)).start();
 				packet =null;
 				//This is the message that the receiver will get after the client object has stopped 
+				System.out.println(temp.trim());
 				if (temp=="Disconnect;"+inds) {
 					break;
 				}
