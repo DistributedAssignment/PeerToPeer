a9226952cc7132678c627fa31bec18408e27f4b4
Luc Murray <lmurray55@qub.ac.uk>
2024-12-07 14:36:56 +0000

    Commit

--- Node.java
+++ Node.java
@@ -689,9 +689,9 @@ public class Node {
 	}
 
 	private class Ping extends Thread {
-		private static int li_ports = new int[2048];
+		private static int[] li_ports = new int[2048];
 		private static int cur_ind = 0;
-		private static InetAddress li_IPs = new InetAddress[2048];
+		private static InetAddress[] li_IPs = new InetAddress[2048];
 		public Ping() {
 			for (int i=0; i<2048;i++){
 				li_ports[i] = 0;
